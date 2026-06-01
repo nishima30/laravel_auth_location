77c46763956f75b3976927814647472af280ba4a
nishima30 <nishima7700@gmail.com>
2026-06-01 22:22:25 +0530

    updated code

--- api/index.php
+++ api/index.php
@@ -1,7 +1,20 @@
 <?php
 
-http_response_code(200);
-header('Content-Type: text/plain');
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 
-echo "API INDEX IS RUNNING";
-exit;
+$dirs = [
+    '/tmp/framework',
+    '/tmp/framework/cache',
+    '/tmp/framework/sessions',
+    '/tmp/framework/views',
+    '/tmp/logs',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->useStoragePath('/tmp');
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
