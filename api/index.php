c3fedc4f42595f210ef7db73a8ad02ee3d3cdd91
nishima30 <nishima7700@gmail.com>
2026-06-01 22:16:14 +0530

    updated code

--- api/index.php
+++ api/index.php
@@ -1,17 +1,7 @@
 <?php
 
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
+http_response_code(200);
+header('Content-Type: text/plain');
 
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (Throwable $e) {
-    http_response_code(500);
-    header('Content-Type: text/plain');
-
-    echo "ERROR CLASS: " . get_class($e) . "\n";
-    echo "ERROR MESSAGE: " . $e->getMessage() . "\n";
-    echo "ERROR FILE: " . $e->getFile() . "\n";
-    echo "ERROR LINE: " . $e->getLine() . "\n\n";
-    echo $e->getTraceAsString();
-}
+echo "API INDEX IS RUNNING";
+exit;
