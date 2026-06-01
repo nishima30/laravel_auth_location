ecd8175d2bfeaec0fe492a8fece0a4e46f49e7bf
nishima30 <nishima7700@gmail.com>
2026-06-01 21:41:36 +0530

    updated code

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (Throwable $e) {
+    http_response_code(500);
+    header('Content-Type: text/plain');
+
+    echo "ERROR CLASS: " . get_class($e) . "\n";
+    echo "ERROR MESSAGE: " . $e->getMessage() . "\n";
+    echo "ERROR FILE: " . $e->getFile() . "\n";
+    echo "ERROR LINE: " . $e->getLine() . "\n\n";
+    echo $e->getTraceAsString();
+}
